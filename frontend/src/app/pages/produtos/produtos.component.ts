e0c64c2008aeb9775389862585f63117771e380f
Elder Galvão <eldergalvao07@gmail.com>
2025-11-11 11:51:50 -0300

    feat: Implementa cadastro de produtos
    - Adiciona formulário para criar novos produtos
    - Implementa funcionalidade completa de CRUD para produtos
    - Formulário inclui validação básica e reset após cadastro
    - Lista é atualizada automaticamente após novo cadastro"

--- frontend/src/app/pages/produtos/produtos.component.ts
+++ frontend/src/app/pages/produtos/produtos.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ProdutoService, Produto } from '../../services/produto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
 })
 export class ProdutosComponent implements OnInit {
   produtos: Produto[] = [];
+  mostrarFormulario = false;
+    novoProduto: Produto = {
+    id: 0,
+    codigo: '',
+    descricao: '',
+    saldo: 0
+  };
 
   constructor(private produtoService: ProdutoService) {}
 
@@ -29,5 +37,23 @@ export class ProdutosComponent implements OnInit {
         console.error('Erro ao carregar produtos:', erro);
       }
     });
+  }
+
+  criarProduto() {
+    this.produtoService.criarProduto(this.novoProduto).subscribe({
+      next: (produtoCriado) => {
+        this.produtos.push(produtoCriado);
+        this.cancelar();
+        console.log('Produto criado:', produtoCriado);
+      },
+      error: (erro) => {
+        console.error('Erro ao criar produto:', erro);
+      }
+    });
+  }
+
+  cancelar() {
+    this.mostrarFormulario = false;
+    this.novoProduto = { id: 0, codigo: '', descricao: '', saldo: 0 };
   }
 }
