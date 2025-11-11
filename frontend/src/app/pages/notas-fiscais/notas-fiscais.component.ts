721d4e7a020843f1089439146cb2a99254b650e9
Elder Galvão <eldergalvao07@gmail.com>
2025-11-11 12:23:37 -0300

    feat: Implementa cadastro básico de notas fiscais
    - Adiciona formulário para criar notas fiscais com status
    - Implementa criação e listagem de notas
    - Numeração sequencial é gerada automaticamente pela API
    - Notas são criadas com status 'Aberta' e lista de itens vazia"

--- frontend/src/app/pages/notas-fiscais/notas-fiscais.component.ts
+++ frontend/src/app/pages/notas-fiscais/notas-fiscais.component.ts
@@ -2,25 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NotaFiscalService, NotaFiscal} from '../../services/nota-fiscal';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notas-fiscais',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './notas-fiscais.component.html',
   styleUrl: './notas-fiscais.component.css'
 })
 export class NotasFiscaisComponent implements OnInit {
   notasFiscais: NotaFiscal[] = [];
+  mostrarFormulario = false;
+  novaNotaFiscal: NotaFiscal = {
+    id: 0,
+    numeracao: '',
+    status: 'Aberta',//status inicial
+    itens: []
+  };
 
-  constructor(private notaService: NotaFiscalService) {}
+  constructor(private notaFiscalService: NotaFiscalService) {}
 
   ngOnInit() {
     this.carregarNotasFiscais();
   }
 
   carregarNotasFiscais() {
-    this.notaService.getNotasFiscais().subscribe({
+    this.notaFiscalService.getNotasFiscais().subscribe({
       next: (notasFiscais) => {
         this.notasFiscais = notasFiscais;
         console.log('Notas Fiscais carregadas:', notasFiscais);
@@ -29,5 +37,28 @@ export class NotasFiscaisComponent implements OnInit {
         console.error('Erro ao carregar Notas Fiscais:', erro);
       }
     });
+  }
+
+  criarNotaFiscal() {
+    this.notaFiscalService.criarNotaFiscal(this.novaNotaFiscal).subscribe({
+      next: (notaCriada: NotaFiscal) => {
+        this.notasFiscais.push(notaCriada);
+        this.cancelar();
+        console.log('Nota fiscal criada:', notaCriada);
+      },
+      error: (erro: any) => {
+        console.error('Erro ao criar nota fiscal:', erro);
+      }
+    });
+  }
+
+  cancelar() {
+    this.mostrarFormulario = false;
+    this.novaNotaFiscal = { 
+      id: 0, 
+      numeracao: '', 
+      status: 'Aberta', 
+      itens: [] 
+    };
   }
 }
